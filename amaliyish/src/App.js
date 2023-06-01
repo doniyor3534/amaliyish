9b84451000a1dbef71072ed8df7e1245e3a3ef10
doniyor3534 <doniyorbektursunov800@gmail.com>
2023-06-01 12:11:50 +0500

    aaaa

---
+++ amaliyish/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './Redux'
+
+
+function App() {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App;
